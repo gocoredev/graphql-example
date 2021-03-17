184236252bbfa4a53f75990884096488c5504ea9
Guilherme <chilo.guilherme@gmail.com>
2021-03-17 00:47:22 -0300

    Added types

--- src/Resolvers/index.ts
+++ src/Resolvers/index.ts
@@ -1,6 +1,15 @@
 import Cars from '../Models/Cars';
 import User from '../Models/Users'
 
+type ICar = {
+    name: String;
+    model: Number;
+}
+type IUser = {
+    name: String;
+    email: String
+}
+
 export = {
     Query: {
         users: ()=>{
@@ -16,12 +25,10 @@ export = {
         }
     },
     Mutation: {
-        // @ts-ignore
-        createUser: (_, {name, email}) =>{
+        createUser: (context: any, {name, email}:IUser) =>{
             return User.create({name, email})
         },
-        // @ts-ignore
-        createCar: (_, {name, model})=>{
+        createCar: (context: any, {name, model}:ICar)=>{
             return Cars.create({
                 name, model
             })
